ed0e8f21d1d044fdc7bec914e863c5d962f520fd
Jarrett Kong <jarrettkong@me.com>
2019-05-30 12:07:33 -0600

    Link to individual restaurant page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchForm from '../../containers/SearchForm/SearchForm';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Yelp from '../../api/Yelp';
 import { addTrending } from '../../actions';
 import RestaurantCard from '../../containers/RestaurantCard/RestaurantCard';
@@ -11,10 +12,13 @@ export class Home extends Component {
 	};
 
 	componentDidMount() {
-		this.setState({ loading: true }, async () => {
-			await this.fetchHotRestaurants();
-			this.setState({ loading: false });
-		});
+		if (!this.props.trending.length) {
+			console.log('loading');
+			this.setState({ loading: true }, async () => {
+				await this.fetchHotRestaurants();
+				this.setState({ loading: false });
+			});
+		}
 	}
 
 	fetchHotRestaurants = async () => {
@@ -34,7 +38,13 @@ export class Home extends Component {
 	};
 
 	render() {
-		const trending = this.props.trending.map(restaurant => <RestaurantCard key={restaurant.id} info={restaurant} />);
+		const trending = this.props.trending.map(restaurant => {
+			return (
+				<Link to={`/restaurant/${restaurant.alias}`} key={restaurant.id}>
+					<RestaurantCard info={restaurant} />
+				</Link>
+			);
+		});
 		return (
 			<section>
 				<h1>Umami</h1>
