c6028630d4d8b32beb9335db9975c4ecf68c0aa8
Jarrett Kong <jarrettkong@me.com>
2019-06-02 11:56:26 -0600

    Update root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { detailsReducer } from './detailsReducer';
 import { resultsReducer } from './resultsReducer';
 import { trendingReducer } from './trendingReducer';
+import { reviewsReducer } from './reviewsReducer';
 
 export const rootReducer = combineReducers({
 	details: detailsReducer,
 	results: resultsReducer,
-	trending: trendingReducer
+	trending: trendingReducer,
+	reviews: reviewsReducer
 });
