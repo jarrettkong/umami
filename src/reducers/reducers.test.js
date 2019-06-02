165949f41e34c830442dec06b8d04fef696a2e1b
Jarrett Kong <jarrettkong@me.com>
2019-06-02 11:07:21 -0600

    Add details and results reducer tests

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,21 +1,54 @@
-import {trendingReducer} from './trendingReducer'
-import * as mockData from '../util/mockData'
-import * as actions from '../actions'
+import { trendingReducer } from './trendingReducer';
+import { resultsReducer } from './resultsReducer';
+import * as mockData from '../util/mockData';
+import * as actions from '../actions';
+import { detailsReducer } from './detailsReducer';
 
 describe('trendingReducer', () => {
-  it('should return the initial state', ()=>{
-    const invalidAction = {
-      type: 'INVALID_ACTION',
-      payload: null
-    }
-    const result = trendingReducer(undefined, invalidAction)
-    expect(result).toEqual([])
-  })
+	it('should return the initial state', () => {
+		const invalidAction = {
+			type: 'INVALID_ACTION',
+			payload: null
+		};
+		const result = trendingReducer(undefined, invalidAction);
+		expect(result).toEqual([]);
+	});
 
-  it('should return a payload of restaurants', ()=>{
-    const result = trendingReducer(undefined, actions.addTrending(mockData.mockResults))
-    expect(result).toEqual(mockData.mockResults)
-  })
-})
+	it('should return an array of restaurants', () => {
+		const result = trendingReducer(undefined, actions.addTrending(mockData.mockResults));
+		expect(result).toEqual(mockData.mockResults);
+	});
+});
 
-describe('resultsReducer')
+describe('resultsReducer', () => {
+	it('should return the initial state', () => {
+		const invalidAction = {
+			type: 'INVALID_ACTION',
+			payload: null
+		};
+		const result = resultsReducer(undefined, invalidAction);
+		expect(result).toEqual([]);
+	});
+
+	it('should return the search results', () => {
+		const result = resultsReducer(undefined, actions.addSearchResults(mockData.mockSearchResult));
+		expect(result).toEqual(mockData.mockSearchResult);
+	});
+});
+
+describe('detailsReducer', () => {
+	it('should return the initial state', () => {
+		const invalidAction = {
+			type: 'INVALID_ACTION',
+			payload: null
+		};
+		const result = detailsReducer(undefined, invalidAction);
+		expect(result).toEqual([]);
+	});
+
+	it('should add the new details to the existing state', () => {
+		const { mockDetails } = mockData;
+		const result = detailsReducer(mockDetails, actions.addDetails(mockDetails[0]));
+		expect(result).toHaveLength(2);
+	});
+});
